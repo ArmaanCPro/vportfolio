9a2ba10c1f468f5bdff94f03a6ca2eddba7a619f
Armaan Chahal <armaan.c2009@gmail.com>
2025-02-12 14:55:29 -0600

    weird change

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -6,7 +6,7 @@ export default function Hero() {
         <section className="py-20 text-center">
             <div className="container mx-auto">
                 <h1 className="text-4xl font-bold mb-4">Welcome to THE Portfolio</h1>
-                <p className="text-xl mb-8">I'm a developer interested in making low-level things and games.</p>
+                <p className="text-xl mb-8">I am a developer interested in making low-level things and games.</p>
                 <Button asChild>
                     <a href="#contact">
                         Get in touch <ArrowRight className="ml-2 h-4 w-4" />
