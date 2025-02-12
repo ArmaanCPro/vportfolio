b4843671602fed895a213821198fc40be96540b4
Armaan Chahal <armaan.c2009@gmail.com>
2025-02-12 16:59:35 -0600

    Revert "make header always on top"
    This reverts commit 0957bc1b96c8028d4f5c217142b9af29454221da.

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button"
 
 export default function Header() {
     return (
-        <header className="fixed top-0 left-0 right-0 z-50 py-6 bg-white shadow-md">
+        <header className="py-6">
             <nav className="container mx-auto flex items-center justify-between">
                 <Link href="/" className="text-2xl font-bold">
                     Armaan Chahal
