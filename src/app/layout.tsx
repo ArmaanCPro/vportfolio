9027d257e7296838c52eb12fb0d73cfece8f8de7
Armaan Chahal <armaan.c2009@gmail.com>
2025-09-05 21:55:50 -0500

    Update deps and add Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import React from "react" // Corrected import for React
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -13,7 +14,11 @@ export const metadata: Metadata = {
 const RootLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+
+        <Analytics />
+      </body>
     </html>
   )
 }
